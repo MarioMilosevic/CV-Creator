1e8076fb5f439a5665eb9946ac75790de0ae6fe9
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-20 17:56:09 +0100

    Style file input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,14 @@
 interface InputFieldType {
-  type: string;
+  type?: string;
   name: string;
 }
 
-const InputField = ({ type, name }: InputFieldType) => {
+const InputField = ({ type = "text", name }: InputFieldType) => {
   return (
     <input
       type={type}
       placeholder={name}
-      className="border-2 border-slate-300 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-200 transition-colors duration-300 px-4 py-2 w-full rounded-md"
+      className="border-2 bg-slate-50 border-slate-300 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-200 transition-colors duration-300 px-4 py-2 w-full rounded-md"
     />
   );
 };
--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -1,45 +1,29 @@
 import InputField from "./InputField";
 const PersonalInfo = () => {
-  const information = [
-    {
-      name: "First Name",
-      type: "text",
-    },
-    {
-      name: "Last Name",
-      type: "text",
-    },
-    {
-      name: "Title",
-      type: "text",
-    },
-    {
-      name: "Photo",
-      type: "file",
-    },
-    {
-      name: "Adress",
-      type: "text",
-    },
-    {
-      name: "Phone number",
-      type: "text",
-    },
-    {
-      name: "email",
-      type: "email",
-    },
-    {
-      name: "Description",
-      type: "text",
-    },
-  ];
   return (
     <>
       <main className="bg-slate-100 py-12 px-4 rounded-md">
         <h2 className="text-2xl mb-4">Personal Information</h2>
+
         <section className="flex flex-col gap-2">
-        {information.map(info => <InputField name={info.name} type={info.type}/>)}
+          <InputField name="First Name" />
+          <InputField name="Last Name" />
+          <InputField name="Title" />
+          <input
+            type="file"
+            id="photo"
+            className="absolute w-[0.1px] h-[0.1px] opacity-0 overflow-hidden z-[-1]"
+          />
+          <label
+            htmlFor="photo"
+            className="border-2 text-slate-400 bg-slate-50 border-slate-300 px-4 py-2 w-full rounded-md "
+          >
+            Photo
+          </label>
+          <InputField name="Adress" />
+          <InputField name="Phone number" />
+          <InputField name="Email" type="email" />
+          <InputField name="Description" />
         </section>
       </main>
     </>
