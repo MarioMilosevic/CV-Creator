671b5afb153a1b3379bfeee137043b5260d7c344
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-20 14:25:44 +0100

    Add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-900 text-gray-100 text-center p-4 text-5xl">
+        CV CREATOR
+    </header>
+  );
+};
+
+export default Header;
