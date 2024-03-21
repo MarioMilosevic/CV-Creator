925e3c55519bbc497c9b8501a5ca55bee109f898
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-21 12:51:55 +0100

    Finish preview with dummy text

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,6 +1,4 @@
-interface PreviewType {
-
-}
+interface PreviewType {}
 
 import profileImage from "../assets/20240321_105523.jpg";
 
@@ -12,46 +10,71 @@ const Preview = () => {
           <h2 className="text-3xl font-semibold">Mario Milosevic</h2>
           <p>Front End Developer React JS</p>
         </header>
-        <main className="w-full">
-          <section className="border border-black w-[70%] pl-4 pr-16 py-4">
+        <main className="w-full flex flex-1">
+          <section className="w-[70%] pl-4 pr-16 py-4">
             <article>
-              <h3 className="semibold text-xl border-b border-b-slate-600">Description</h3>
-              <p>
+              <h3 className="font-semibold text-green-500 text-xl border-b border-b-slate-600">
+                Description
+              </h3>
+              <p className="py-2 italic pr-2">
                 Visok 185cm tezak nekih 97kg iz mrtvog 220kg benc 135kg cucanj
                 170kg total, 525kg
               </p>
             </article>
             <article>
-              <h3>Education</h3>
-              <div>
-                <p>20/09/2015 - 01/06/2019</p>
+              <h3 className="font-semibold text-green-500 text-xl border-b border-b-slate-600">
+                Education
+              </h3>
+              <div className="flex gap-24 pr-2 py-2">
+                <p className="font-semibold">20/09/2015 - 01/06/2019</p>
                 <div>
-                  <p>Bachelor of Marine Science</p>
+                  <p className="font-semibold">Bachelor of Marine Science</p>
                   <p>Marine Science</p>
                   <p>Marine Faculty of Kotor, Kotor</p>
                 </div>
               </div>
             </article>
             <article>
-              <h3>Experience</h3>
-              <div>
-                <p>06/06/2018 - 30/09/2018</p>
+              <h3 className="font-semibold text-green-500 text-xl border-b border-b-slate-600">
+                Experience
+              </h3>
+              <div className="flex gap-24 pr-2 py-2">
+                <p className="font-semibold">20/09/2015 - 01/06/2019</p>
                 <div>
-                  <p>Front End Developer</p>
-                  <p>Njuskalo HR, Zagreb</p>
+                  <p className="font-semibold">Front End Developer</p>
+                  <p>Quantox Tehnologies</p>
+                  <p>Marine Faculty of Kotor, Kotor</p>
                 </div>
               </div>
             </article>
           </section>
-          <aside>
-            <img src={profileImage} alt="Profile Picture" className="w-[300px]"/>
-            <h2>Personal Details</h2>
-            <h3>Adress</h3>
-            <p>Patrijarha Rajacica 39 Novi Sad</p>
-            <h2>Phone Number</h2>
-            <p>069888799</p>
-            <h3>Email</h3>
-            <p>mariomilosevic@gmail.com</p>
+          <aside className="w-[30%] bg-slate-200 flex-1">
+            <div className="h-full flex flex-col">
+              <img
+                src={profileImage}
+                alt="Profile Picture"
+                className="w-full object-cover"
+              />
+              <div className="p-2">
+                <h2 className="font-semibold text-green-500 text-xl border-b border-b-slate-600">
+                  Personal Details
+                </h2>
+                <div className="flex flex-col gap-2 py-2">
+                  <div>
+                    <h2 className="font-semibold ">Adress</h2>
+                    <p>Patrijarha Rajacica 39 Novi Sad</p>
+                  </div>
+                  <div>
+                    <h2 className="font-semibold ">Phone Number</h2>
+                  </div>
+                  <p>069888799</p>
+                  <div>
+                    <h2 className="font-semibold ">Email</h2>
+                    <p>mariomilosevic@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </aside>
         </main>
       </div>
